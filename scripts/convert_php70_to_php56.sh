56059701d77b9fe9c750b9898cfb9cae51f3614b
Tyson Andre <tysonandre775@hotmail.com>
2017-05-07 13:44:58 -0700

    Fix scripts

--- scripts/convert_php70_to_php56.sh
+++ scripts/convert_php70_to_php56.sh
@@ -4,10 +4,15 @@ if [ -z "$TRANSPHPILE_PATH" ]; then
 	exit 1
 fi
 	
-for foo in src/**/*.php; do
-    if php -d memory_limit=500M $TRANSPHPILE_PATH transpile "$foo" ; then
-		cp "php5/$foo" "$foo"
+# Note: Add -v after the transpile command to show stack traces if this fails
+for filepath in src/**/*.php; do
+	if grep -q 'This code has been transpiled via TransPHPile\. For more information' "$filepath"; then
+		echo "Skipping '$filepath', already transpiled"
+		continue
+	fi
+    if php -d memory_limit=500M $TRANSPHPILE_PATH transpile "$filepath" ; then
+		cp "php5/$filepath" "$filepath"
 	else
-		echo "Failed to transpile $foo";
+		echo "Failed to transpile '$filepath'";
 	fi
 done
