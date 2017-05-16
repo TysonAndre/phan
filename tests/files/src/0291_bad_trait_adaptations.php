50e4113cde82805e8ad6c9b0ad5afd2fcaadcfe6
Tyson Andre <tandre@themeetgroup.com>
2017-05-16 15:46:11 -0700

    Fix a crash when using short form trait method aliasing

--- tests/files/src/0291_bad_trait_adaptations.php
+++ tests/files/src/0291_bad_trait_adaptations.php
@@ -4,6 +4,7 @@
 trait Trait291 {
     public function baz() { }
     public function xyz() { }
+    public function shortform(int $x) { }
 }
 trait Trait291B {
 }
@@ -13,6 +14,9 @@ class A291 {
         Trait291::foo as bar;
         Trait291B::xyz insteadof Trait291;
         Trait291C::zz as zzAlias;
+        shortform as shortformalias;
+        shortform as private shortformaliasPrivate;  // FIXME track visibility of trait method aliases
+        shortform2 as shortformAliasOfMissing;
     }
 }
 
@@ -21,4 +25,11 @@ function test291() {
     $x->baz();
     $x->bar();
     $x->foo();
+    $x->shortform(2);
+    $x->shortform([2]);
+    $x->shortformalias(2);
+    $x->shortformalias([2]);
+    $x->shortformaliasPrivate(2);  // FIXME should warn about calling private method
+    $x->shortform2();
+    $x->shortformAliasOfMissing();
 }
