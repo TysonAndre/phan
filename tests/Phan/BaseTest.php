d32b3d51fec37a5c9a2ec137f5c37d332db00932
Tyson Andre <tysonandre775@hotmail.com>
2017-06-03 18:40:08 -0700

    Update phpunit from 5.7 to latest stable 6.2
    PHPUnit supports 6.2, and is more likely to have up to date
    documentation, bug fixes, code coverage generation, etc.

--- tests/Phan/BaseTest.php
+++ tests/Phan/BaseTest.php
@@ -2,11 +2,12 @@
 
 namespace Phan\Tests;
 
+use PHPUnit\Framework\TestCase;
 /**
  * Any common initialization or configuration should go here
  * (E.g. may want to change https://phpunit.de/manual/current/en/fixtures.html#fixtures.global-state in some classes)
  */
-class BaseTest extends \PHPUnit_Framework_TestCase {
+class BaseTest extends TestCase {
     // Needed to prevent phpunit from backing up these private static variables.
     // See https://phpunit.de/manual/current/en/fixtures.html#fixtures.global-state
     protected $backupStaticAttributesBlacklist = [
