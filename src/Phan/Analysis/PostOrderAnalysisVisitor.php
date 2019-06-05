f2f4671fc5a184dae446e98c942bac975c37724f
Tyson Andre <tysonandre775@hotmail.com>
2019-06-05 09:50:24 -0400

    Continue implementing this

--- src/Phan/Analysis/PostOrderAnalysisVisitor.php
+++ src/Phan/Analysis/PostOrderAnalysisVisitor.php
@@ -2319,7 +2319,7 @@ class PostOrderAnalysisVisitor extends AnalysisVisitor
 
             if (!$possible_ancestor_type->isEmpty()) {
                 // but forbid 'self::__construct', 'static::__construct'
-                $type = $this->context->getClassFQSEN()->asUnionType();
+                $type = $this->context->getClassFQSEN()->asRealUnionType();
                 if ($possible_ancestor_type->hasStaticType()) {
                     $this->emitIssue(
                         Issue::AccessOwnConstructor,
